d6d05f24b105ce6c14b2eb584def8c9a0acb61cd
fischermatte <fischermatte@posteo.net>
2015-10-14 15:33:47 +0200

    initial project setup

---
+++ src/main/java/com/geolud/BoottestApplication.java
@@ -0,0 +1,12 @@
+package com.geolud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoottestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoottestApplication.class, args);
+    }
+}
