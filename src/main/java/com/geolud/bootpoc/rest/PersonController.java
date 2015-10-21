6c116d4d75c9821521997939a5d432506ff356c3
fischermatte <fischermatte@posteo.net>
2015-10-21 11:00:10 +0200

    updated to 1.3.0 RC1

---
+++ src/main/java/com/geolud/bootpoc/rest/PersonController.java
@@ -0,0 +1,7 @@
+package com.geolud.bootpoc.rest;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PersonController {
+}
--- src/main/java/com/geolud/boottest/web/HomeController.java
+++ src/main/java/com/geolud/boottest/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.geolud;
+package com.geolud.boottest.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
