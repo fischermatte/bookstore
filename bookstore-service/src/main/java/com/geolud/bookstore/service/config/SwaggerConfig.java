a52984a9f2c23c956b4c4caee5624e8c075688e1
fischermatte <fischermatte@posteo.net>
2015-12-02 11:39:52 +0100

    enabled swagger

--- bookstore-service/src/main/java/com/geolud/bookstore/service/config/SwaggerConfig.java
+++ bookstore-service/src/main/java/com/geolud/bookstore/service/config/SwaggerConfig.java
@@ -7,46 +7,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
-//    @Autowired
-//    private TypeResolver typeResolver;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-//    @Bean
-//    public Docket bookApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build()
-//                .pathMapping("/")
-//                .directModelSubstitute(LocalDate.class,
-//                        String.class)
-//                .genericModelSubstitutes(ResponseEntity.class)
-//                .alternateTypeRules(
-//                        newRule(typeResolver.resolve(DeferredResult.class,
-//                                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
-//                                typeResolver.resolve(WildcardType.class)))
-//                .useDefaultResponseMessages(false)
-//                .globalResponseMessage(RequestMethod.GET,
-//                        newArrayList(new ResponseMessageBuilder()
-//                                .code(500)
-//                                .message("500 message")
-//                                .responseModel(new ModelRef("Error"))
-//                                .build()))
-//                .enableUrlTemplating(true);
-//    }
-
-//    List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope
-//                = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return newArrayList(
-//                new SecurityReference("mykey", authorizationScopes));
-//    }
 }
