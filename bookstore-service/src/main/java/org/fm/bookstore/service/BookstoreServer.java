b403cfced1827c2dd8516c2331d1ae07fd43c806
fischermatte <fischermatte@posteo.net>
2016-01-14 10:18:57 +0100

    added missing welcome page config

--- bookstore-service/src/main/java/org/fm/bookstore/service/BookstoreServer.java
+++ bookstore-service/src/main/java/org/fm/bookstore/service/BookstoreServer.java
@@ -2,11 +2,12 @@ package org.fm.bookstore.service;
 
 import org.fm.bookstore.service.config.RestConfig;
 import org.fm.bookstore.service.config.SwaggerConfig;
+import org.fm.bookstore.service.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(value = {RestConfig.class, SwaggerConfig.class})
+@Import(value = {RestConfig.class, SwaggerConfig.class, WebConfig.class})
 @SpringBootApplication
 public class BookstoreServer {
 
