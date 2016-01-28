44d00e77ca614c3c8ecdef83d85008a8998e2772
fischermatte <fischermatte@posteo.net>
2016-01-28 22:32:32 +0100

    renamed integration test

--- bookstore-inventory/src/test/java/org/fischermatte/bookstore/inventory/BookstoreInventoryServerIT.java
+++ bookstore-inventory/src/test/java/org/fischermatte/bookstore/inventory/BookstoreInventoryServerIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @DefaultIntegrationTest
-public class BookstoreInventoryServerTest {
+public class BookstoreInventoryServerIT {
 
 	@Test
 	public void contextLoads() {
