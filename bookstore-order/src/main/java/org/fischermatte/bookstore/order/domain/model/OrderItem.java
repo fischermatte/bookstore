4da8ac16320eccfb2070996f7bdc4239a244b768
fischermatte <fischermatte@posteo.net>
2016-02-09 23:57:35 +0100

    #17 replaced Persistence.generateSchema() by hibernates SchemaExport

--- bookstore-order/src/main/java/org/fischermatte/bookstore/order/domain/model/OrderItem.java
+++ bookstore-order/src/main/java/org/fischermatte/bookstore/order/domain/model/OrderItem.java
@@ -1,4 +1,17 @@
 package org.fischermatte.bookstore.order.domain.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private UUID id;
 }
