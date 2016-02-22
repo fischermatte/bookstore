20a364c27e8530aafa8a07e3199f5293c708d8f0
fischermatte <fischermatte@posteo.net>
2016-02-22 20:19:02 +0100

    domain events are now persisted

--- bookstore-order/src/main/java/org/fischermatte/bookstore/order/infrastructure/DomainEventListener.java
+++ bookstore-order/src/main/java/org/fischermatte/bookstore/order/infrastructure/DomainEventListener.java
@@ -2,27 +2,25 @@ package org.fischermatte.bookstore.order.infrastructure;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Component
 public class DomainEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(DomainEventListener.class);
 
-    private final DomainEventRepository domainEventRepository;
-
-    @Autowired
-    public DomainEventListener(DomainEventRepository domainEventRepository) {
-        this.domainEventRepository = domainEventRepository;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     private void onDomainEvent(DomainEvent event){
         logger.debug("persisting domain event {}", event);
-        domainEventRepository.save(event);
+        entityManager.persist(event);
     }
 
 }
