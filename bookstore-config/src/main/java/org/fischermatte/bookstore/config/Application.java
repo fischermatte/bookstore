dee04de463ed1bc1e202ccdeef8ebc38697af86e
fischermatte <fischermatte@posteo.net>
2016-03-01 00:18:09 +0100

    fixed spring version dependency issue

--- bookstore-config/src/main/java/org/fischermatte/bookstore/config/Application.java
+++ bookstore-config/src/main/java/org/fischermatte/bookstore/config/Application.java
@@ -2,8 +2,12 @@ package org.fischermatte.bookstore.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
